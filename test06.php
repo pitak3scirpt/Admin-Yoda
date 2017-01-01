a3ee85a713e5f9fb18ed3386a65c99598782fddf
Jedi Council <pitak3g.sc@gmail.com>
2017-01-01 17:14:59 +0700

    Update test06.php

--- test06.php
+++ test06.php
@@ -1,4 +1,4 @@
 <?php
-$array = array(0 => ‘blue’, 1 => ‘red’, 2 => ‘green’, 3 => ‘red’);
-echo array_search(‘green’, $array);
+$array = array(0 => 'blue', 1 => 'red', 2 => 'green', 3 => 'red');
+echo array_search('green', $array);
 ?>
