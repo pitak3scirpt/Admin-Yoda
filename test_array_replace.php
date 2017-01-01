49f42bea112246242029a91479668598e4d87e93
Jedi Council <pitak3g.sc@gmail.com>
2017-01-01 17:37:48 +0700

    Update test_array_replace.php

--- test_array_replace.php
+++ test_array_replace.php
@@ -1,5 +1,8 @@
 <?php
 $base = array("orange", "banana", "apple", "raspberry");
+var_dump($basket);
+echo "<br>";
+echo "<br>";
 $replacements = array(0 => "pineapple", 4 => "cherry");
 $basket = array_replace($base, $replacements);
 var_dump($basket);
