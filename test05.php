aa01932071eae6c147273b78459f88bab4078450
Jedi Council <pitak3g.sc@gmail.com>
2017-01-01 17:01:40 +0700

    Create test05.php

---
+++ test05.php
@@ -0,0 +1,15 @@
+<?php
+foreach (range(0, 12) as $number) {
+  echo $number;
+}
+  echo “<br>”;
+// การเพิ่มทีละขั้น
+foreach (range(0, 100, 10) as $number) {
+  echo $number;
+}
+echo “<br>”;
+// การใช้ตัวอักษร
+foreach (range(‘a’, ‘i’) as $letter) {
+  echo $letter;
+}
+?>
