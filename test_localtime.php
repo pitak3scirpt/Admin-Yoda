3f7f315cb3fef6691e3c500e375a2caa7955b6b0
Jedi Council <pitak3g.sc@gmail.com>
2017-01-01 19:56:33 +0700

    Update test_localtime.php

--- test_localtime.php
+++ test_localtime.php
@@ -5,6 +5,8 @@ echo time();
 echo "<br>";
 echo date('Y-m-d',time());
 echo "<br>";
+echo date("M-d-Y", mktime(0, 0, 0, 1, 1, 1997));
+echo "<br>";
 $localtime = localtime();
 echo '<pre>';
 var_dump($localtime);
