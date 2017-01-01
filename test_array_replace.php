7edeadf0c4f3f0e525b871dc75822b0674802eb6
Jedi Council <pitak3g.sc@gmail.com>
2017-01-01 17:36:05 +0700

    Create test_array_replace.php

---
+++ test_array_replace.php
@@ -0,0 +1,6 @@
+<?php
+$base = array("orange", "banana", "apple", "raspberry");
+$replacements = array(0 => "pineapple", 4 => "cherry");
+$basket = array_replace($base, $replacements);
+var_dump($basket);
+?>
