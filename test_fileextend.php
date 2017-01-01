8ae15bf91a2f414d59abc3b2816fd03573b531ac
Jedi Council <pitak3g.sc@gmail.com>
2017-01-01 20:21:14 +0700

    Update test_fileextend.php

--- test_fileextend.php
+++ test_fileextend.php
@@ -7,4 +7,6 @@ $dir = new DirectoryIterator(dirname(FILE));
 foreach ($dir as $fileinfo) {
   echo $fileinfo->getFilename() . "<br>";
 }
+$iterator = new DirectoryIterator(dirname(FILE));
+echo $iterator->getPath();
 ?>
