7798c397e8f703e3dbaf1471708de6f3fd2f2dbf
Jedi Council <pitak3g.sc@gmail.com>
2017-01-01 19:52:05 +0700

    Update test_localtime.php

--- test_localtime.php
+++ test_localtime.php
@@ -1,6 +1,10 @@
 <?php
 echo "Today : ".date('F jS, Y', strtotime('today'));
 echo "<br>";
+echo time();
+echo "<br>";
+echo date('Y-m-d',time());
+echo "<br>";
 $localtime = localtime();
 echo '<pre>';
 var_dump($localtime);
